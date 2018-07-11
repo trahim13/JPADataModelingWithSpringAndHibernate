9e4516189f276e2928999dc1e3ca0852f847a715
trahim13 <trachimovich@gmail.com>
2018-07-11 04:03:57 +0300

    Spring Data JPA Query Methods. Example.

--- src/main/java/org/trahim/spring/controllers/IndexController.java
+++ src/main/java/org/trahim/spring/controllers/IndexController.java
@@ -1,4 +1,29 @@
 package org.trahim.spring.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.trahim.spring.domain.Category;
+import org.trahim.spring.repositories.CategoryRepository;
+import org.trahim.spring.repositories.UnitOfMeasureRepository;
+
+import java.util.Optional;
+
+@Controller
 public class IndexController {
+
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage() {
+        Optional<Category> category = categoryRepository.findByDescription("dddddd");
+        System.out.println(category.get().getId());
+
+        return "index";
+    }
 }
--- src/main/java/org/trahim/spring/repositories/UnitOfMeasureRepository.java
+++ src/main/java/org/trahim/spring/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package org.trahim.spring.repositories;
 import org.springframework.data.repository.CrudRepository;
 import org.trahim.spring.domain.UnitOfMeasure;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String s);
 }
